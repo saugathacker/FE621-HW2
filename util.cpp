fe6326ee7ac9e3a1bc66dd88fa1b34e3d52d0d4e
saugathacker <saugathacker@gmail.com>
2025-02-19 20:32:58 -0500

    absolute_error function

--- util.cpp
+++ util.cpp
@@ -1,6 +1,7 @@
 #include "util.h"
 #include <cmath>
 #include <numbers>
+#include <array>
 
 // Implementation of the Normal CDF function
 double norm_cdf(double x)
@@ -12,4 +13,19 @@ double norm_cdf(double x)
 double norm_pdf(double x)
 {
     return std::exp(-0.5 * x * x) / std::sqrt(2 * std::numbers::pi);
+}
+
+std::unordered_map<int, double> absolute_error(BSM &bs, BinomialTree &bt)
+{
+    std::array<int, 12> steps_list = {10, 20, 30, 40, 50, 100, 150, 200, 250, 300, 350, 400};
+    std::unordered_map<int, double> result;
+    double vol = 0.4;
+    // iterate all the choices for steps
+    for (int steps : steps_list)
+    {
+        double error = std::fabs(bs(vol) - bt(steps));
+        result[steps] = error;
+    }
+
+    return result;
 }
--- util.h
+++ util.h
@@ -1,7 +1,12 @@
 #pragma once
+#include <unordered_map>
+#include "BSM.h"
+#include "BinomialTree.h"
 
 // Normal CDF function (declaration)
 double norm_cdf(double x);
 
 // Normal PDF function
 double norm_pdf(double x);
+
+std::unordered_map<int, double> absolute_error(BSM &bs, BinomialTree &bt);
